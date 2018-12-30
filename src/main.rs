1d329184becfa8418a2a9d1fa61895fa8d28f154
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2018-12-30 11:27:42 -0600

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ fn main() {
     pretty_env_logger::init();
 
     // Match any request and return hello world!
-    let routes = warp::any()
-        .map(|| "Hello, World!")
-	.with(warp::log("main"));
+    let routes = warp::any().map(|| "Hello, World!").with(warp::log("main"));
 
     warp::serve(routes)
         .tls("localhost-cert.pem", "localhost-privkey.pem")
