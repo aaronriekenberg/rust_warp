872aee4553c881f9a74a82edfc22af878a6e3092
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2018-12-30 11:24:51 -0600

    Add code.

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use warp::Filter;
+
+fn main() {
+    pretty_env_logger::init();
+
+    // Match any request and return hello world!
+    let routes = warp::any()
+        .map(|| "Hello, World!")
+	.with(warp::log("main"));
+
+    warp::serve(routes)
+        .tls("localhost-cert.pem", "localhost-privkey.pem")
+        .run(([127, 0, 0, 1], 3030));
+}
